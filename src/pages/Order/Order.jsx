c208c2d6fada967aa7006efc5e9c419f571d491a
Z <zzolboo761@gmail.com>
2025-03-19 11:14:58 +0800

    up

--- src/pages/Order/Order.jsx
+++ src/pages/Order/Order.jsx
@@ -1,30 +1,31 @@
 import React, { useState } from "react";
 import Order from "../../components/order/Order";
-import { useCart } from "../../context/CartContext";
 
 const OrderForm = () => {
-  const [Active, setActive] = useState(true);
+  const [activeStep, setActiveStep] = useState(1); // Track the active step dynamically
 
-  const active =
-    "size-[45px] shadow-xl rounded-full bg-[#feb6bb] text-lg flex justify-center items-center";
-  const deactive =
-    "size-[45px] shadow-inner shadow-slate-300 rounded-full text-lg flex justify-center items-center";
+  const steps = [1, 2, 3, 4]; // Array of steps
+  const getStepClass = (step) => {
+    const baseClass =
+      "w-[45px] h-[45px] text-lg flex justify-center items-center rounded-full";
+    return step === activeStep
+      ? `${baseClass} bg-[#feb6bb] shadow-xl` // Active step styles
+      : `${baseClass} shadow-inner shadow-slate-300`; // Inactive step styles
+  };
+
   return (
     <div>
       <div className="flex justify-center items-center p-10">
-        <div className="size-[45px] shadow-inner shadow-slate-300 rounded-full text-lg flex justify-center items-center">
-          1
-        </div>
-        <span className="w-[50px] h-[1px] bg-black flex items-center align-center"></span>
-        <div className={Active ? active : deactive}>2</div>
-        <span className="w-[50px] h-[1px] bg-black flex items-center align-center"></span>
-        <div className="size-[45px] shadow-inner shadow-slate-300 rounded-full text-lg flex justify-center items-center">
-          3
-        </div>
-        <span className="w-[50px] h-[1px] bg-black flex items-center align-center"></span>
-        <div className="size-[45px] shadow-inner shadow-slate-300 rounded-full text-lg flex justify-center items-center">
-          4
-        </div>
+        {steps.map((step, index) => (
+          <React.Fragment key={step}>
+            {/* Render step */}
+            <div className={getStepClass(step)}>{step}</div>
+            {/* Render separator line except after the last step */}
+            {index < steps.length - 1 && (
+              <span className="w-[50px] h-[1px] bg-black mx-2"></span>
+            )}
+          </React.Fragment>
+        ))}
       </div>
       <Order />
     </div>
