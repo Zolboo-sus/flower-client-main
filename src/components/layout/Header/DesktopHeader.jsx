1f874fc6251ef6b64a79c061bf6f40d831f21ed5
Z <zzolboo761@gmail.com>
2025-03-21 12:03:03 +0800

    up

--- src/components/layout/Header/DesktopHeader.jsx
+++ src/components/layout/Header/DesktopHeader.jsx
@@ -11,7 +11,6 @@ const DesktopHeader = () => {
   const menuItems = [
     { name: "Баглаа", id: "occasion" },
     { name: "Flowers", id: "flowers" },
-  
     { name: "Plants", id: "plants" },
     { name: "Gifts", id: "gift" },
     { name: "Sale", id: "d" },
@@ -20,7 +19,7 @@ const DesktopHeader = () => {
   return (
     <div className="flex pt-8 mb-[160px] justify-between pb-5 border-b-2 border-[#ffd1d4] fixed top-0 left-0 w-full z-50 bg-white px-5">
       <div className="flex w-[15%] h-auto border-1 border-white bg-[#ffedee] rounded-lg shadow-inner items-center">
-        <img src={logo} alt="Logo" className="px-2 h-[3vw] my-1" />
+        <img src={logo} alt="Flower Center Mongolia Logo" className="px-2 h-[3vw] my-1" />
         <a href="/">
           <span className="w-full text-[0.8vw] uppercase font-serif my-1 cursor-pointer">
             flower center mongolia
@@ -29,18 +28,17 @@ const DesktopHeader = () => {
       </div>
 
       <div className="flex flex-col items-center justify-center w-[65%] rounded-lg px-5 mr-3">
-        <div className="flex w-full items-center justify-between shadow-inner-[#d3d3d3] inner-nav ">
+        <div className="flex w-full items-center justify-between shadow-inner">
           {menuItems.map((item, index) => (
             <span
               key={index}
-              className={`text-[1.2vw] font-medium cursor-pointer px-4 py-2 text-gray-700 hover:text-[#ffd1d4] ${activeIndex === index
+              className={`text-[1.2vw] md:text-[1.5vw] lg:text-[1.8vw] font-medium cursor-pointer px-4 py-2 text-gray-700 hover:text-[#ffd1d4] ${activeIndex === index
                 ? "underline underline-offset-4 decoration-[#ffd1d4]"
                 : ""
                 }`}
               onClick={() => setActiveIndex(index)}
             >
               <Link
-                activeClass="active"
                 to={item.id}
                 spy={true}
                 smooth={true}
@@ -51,14 +49,11 @@ const DesktopHeader = () => {
               </Link>
             </span>
           ))}
-          <a href="/cart">
+          <Link to="/cart">
             <div className="flex items-center justify-center">
-              <AiOutlineShopping
-                className="cursor-pointer hover:text-[#ffd1d4] 
-              text-[2vw]"
-              />
+              <AiOutlineShopping className="cursor-pointer hover:text-[#ffd1d4] text-[2vw]" />
             </div>
-          </a>
+          </Link>
         </div>
       </div>
     </div>
