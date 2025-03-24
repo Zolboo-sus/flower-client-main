1839776862094558f21ca4c1a99fcc37740a2416
Z <zzolboo761@gmail.com>
2025-03-24 21:29:47 +0800

    up

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -10,34 +10,36 @@ const Cart = () => {
   const [isLoading, setIsLoading] = useState(false);
 
   const handleIncrement = (id, quality) => {
-    updateCartItem({ id, quality });
+    if (quality < 99) {
+      updateCartItem({ id, quality: quality + 1 });
+    }
   };
 
-
   const handleDecrement = (id, quality) => {
-    if (quality > 0) {
-      updateCartItem({ id, quality });
+    if (quality > 1) {
+      updateCartItem({ id, quality: quality - 1 });
     } else {
       removeFromCart(id);
     }
   };
 
-
-  const totalAmount = cart ? cart.reduce(
+  const totalAmount = cart?.reduce(
     (acc, item) => acc + item.price * item.quality,
     0
-  ) : 0;
+  ) ?? 0;
 
   const invoiceSend = () => {
     setIsLoading(true);
 
-    axios.post('https://tsetsegtuw.templateapi.xyz/tsetsegtuv/invoice', {
-      price: totalAmount
-    }).then((e) => {
-      navigate('/order/' + e.data.data.id);
-    })
+    axios
+      .post("https://tsetsegtuw.templateapi.xyz/tsetsegtuv/invoice", {
+        price: totalAmount,
+      })
+      .then((e) => {
+        navigate("/order/" + e.data.data.id);
+      })
       .finally(() => setIsLoading(false));
-  }
+  };
 
   return (
     <div className="flex flex-col p-6 mx-auto max-w-4xl">
@@ -53,7 +55,9 @@ const Cart = () => {
               {/* Product Info */}
               <div className="flex items-center space-x-4">
                 <img
-                  src={'https://tsetsegtuw.templateapi.xyz/tsetsegtuv/' + item.image}
+                  src={`https://tsetsegtuw.templateapi.xyz/tsetsegtuv/${
+                    item.image || "default_image.jpg"
+                  }`}
                   alt={item.productName}
                   className="w-20 h-20 object-cover rounded-md shadow-md"
                 />
@@ -63,10 +67,10 @@ const Cart = () => {
                 </div>
               </div>
 
-              {/* quality Controls */}
+              {/* Quality Controls */}
               <div className="flex items-center space-x-4">
                 <button
-                  onClick={() => handleDecrement(item.id, item.quality - 1)}
+                  onClick={() => handleDecrement(item.id, item.quality)}
                   className="px-2 py-1 border border-gray-300 rounded-md hover:bg-gray-200"
                 >
                   -
@@ -75,7 +79,7 @@ const Cart = () => {
                   {item.quality}
                 </span>
                 <button
-                  onClick={() => handleIncrement(item.id, item.quality + 1)}
+                  onClick={() => handleIncrement(item.id, item.quality)}
                   className="px-2 py-1 border border-gray-300 rounded-md hover:bg-gray-200"
                 >
                   +
@@ -84,7 +88,7 @@ const Cart = () => {
 
               {/* Product Total */}
               <div className="text-lg font-semibold">
-                {Intl.NumberFormat('en-us').format(item.price * item.quality)}₮
+                {Intl.NumberFormat("mn-MN").format(item.price * item.quality)}₮
               </div>
 
               <button
@@ -98,7 +102,9 @@ const Cart = () => {
 
           <div className="flex justify-between items-center border-t border-gray-300 pt-4 mt-6">
             <h3 className="text-xl font-bold">Нийт:</h3>
-            <span className="text-2xl font-semibold">{Intl.NumberFormat('en-us').format(totalAmount)}₮</span>
+            <span className="text-2xl font-semibold">
+              {Intl.NumberFormat("mn-MN").format(totalAmount)}₮
+            </span>
           </div>
         </div>
       ) : (
@@ -107,10 +113,16 @@ const Cart = () => {
 
       {cart && cart.length > 0 && (
         <div className="mt-6">
-          {isLoading ? <LoaderButton /> :
-            <button onClick={() => invoiceSend()} className="w-full px-10 bg-pink-500 text-white py-3 rounded-md text-lg font-semibold hover:bg-pink-600">
+          {isLoading ? (
+            <LoaderButton />
+          ) : (
+            <button
+              onClick={() => invoiceSend()}
+              className="w-full px-10 bg-pink-500 text-white py-3 rounded-md text-lg font-semibold hover:bg-pink-600"
+            >
               Үргэлжлүүлэх
-            </button>}
+            </button>
+          )}
         </div>
       )}
     </div>
