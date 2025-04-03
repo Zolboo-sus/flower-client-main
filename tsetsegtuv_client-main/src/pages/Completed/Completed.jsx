03a2aa608dd24800d77d9ba3448d6386648cbe19
Khln12 <satsujin9@gmail.com>
2025-04-03 23:31:35 +0800

    up

--- tsetsegtuv_client-main/src/pages/Completed/Completed.jsx
+++ tsetsegtuv_client-main/src/pages/Completed/Completed.jsx
@@ -1,33 +1,19 @@
-import React, { useState } from "react";
+import React from "react";
 import Header from "../../components/layout/Header/Header";
-import DesktopHeader from "../../components/layout/Header/DesktopHeader";
+import { useState } from "react";
+// import Footer from "../../components/layout/Footer/Footer";
+// import Card from "../../components/card/Card";
+// import Button from "../../components/Button/Button";
 import flower from "../../assets/flower.png";
+import DesktopHeader from "../../components/layout/Header/DesktopHeader";
 
 const Completed = () => {
-  // Энд захиалагчийн мэдээлэл хадгалагдсан formData
-  const [formData, setFormData] = useState({
-    firstName: "",
-    lastName: "",
-    companyReg: "",
-    companyName: "",
-    phone: "",
-    email: "",
-    district: "",
-    subdistrict: "",
-    address: "",
-    receiverName: "",
-    receiverLastName: "",
-    receiverPhone: "",
-    deliveryTime: "",
-    message: "",
-    price: 0, // Энэ нь totalAmount-оос ирэх болно
-    orders: [], // cart мэдээлэлтэй
-  });
+  const [Active, setActive] = useState(true);
 
-  // Захиалагчийн нэр болон утасны дугаарыг харуулах
-  const customerName = `${formData.firstName} ${formData.lastName}`;
-  const customerPhone = formData.phone;
-
+  const active =
+    "size-[45px] shadow-xl rounded-full bg-[#feb6bb] text-lg flex justify-center items-center";
+  const deactive =
+    "size-[45px] shadow-inner shadow-slate-300 rounded-full text-lg flex justify-center items-center";
   return (
     <div>
       {window.innerWidth < 720 ? (
@@ -43,7 +29,6 @@ const Completed = () => {
           <DesktopHeader />{" "}
         </div>
       ) : null}
-
       <div className="flex justify-center items-center p-10">
         <div className="size-[45px] shadow-inner shadow-slate-300 rounded-full text-lg flex justify-center items-center">
           1
@@ -57,16 +42,12 @@ const Completed = () => {
           3
         </div>
         <span className="w-[50px] h-[1px] bg-black flex items-center align-center"></span>
-        <div className="size-[45px] shadow-inner shadow-slate-300 rounded-full text-lg flex justify-center items-center">
-          4
-        </div>
+        <div className={Active ? active : deactive}>4</div>
       </div>
-
       <div>
         <span className="text-xl font-semibold text-center my-4 lg:text-[2vw]">
           Таны захиалга амжилттай баталгаажлаа💖
         </span>
-
         <div className="md:mx-[2v%] m-[5%] md:p-[2%] p-[5%]  shadow-xl md:flex ">
           <div className="md:w-[40%] ">
             <div className="imgs flex justify-evenly flex-wrap md:mb-[2%]">
@@ -80,20 +61,19 @@ const Completed = () => {
               </div>
             </div>
           </div>
-
           <div className="medeelel md:w-[60%]  ">
             <div className="w-full rounded-lg px-2 py-1 mb-[3%]">
               <div className="bg-[#f2f3f2] py-2 rounded-lg text-left md:text-[24px] text-[18px] px-3 shadow-lg shadow-[#fcd7d7]">
-                <p className="font-medium">Нийт төлсөн дүн: {formData.price}₮</p>
+                <p className="font-medium">Нийт төлсөн дүн: 376,000₮</p>
               </div>
             </div>
             <div className="w-full rounded-lg px-2 py-1 text-left md: mb-[3%]">
               <div className="md:font-medium font-normal bg-[#f2f3f2] rounded-lg md:text-[24px] text-[14px]  text-wrap px-3 py-5 space-y-2 shadow-lg shadow-[#fcd7d7]">
-                <p>Хүлээн авагч: {formData.receiverName}</p>
-                <p>Хүлээн авагчийн утасны дугаар: {formData.receiverPhone}</p>
-                <p>Хүлээн авагчийн хаяг: {formData.address}</p>
-                <p>Захиалагч: {customerName}</p>
-                <p>Захиалагчийн утасны дугаар: {customerPhone}</p>
+                <p>Хүлээн авагч: Энэрэл</p>
+                <p>Хүлээн авагчийн утасны дугаар: 85855855</p>
+                <p>Хүлээн авагчийн хаяг: Baga Toiruu, Chingis Museum</p>
+                <p>Захиалагч: Золбоотбаатар</p>
+                <p>Захиалагчийн утасны дугаар: 83948759</p>
               </div>
             </div>
             <div className="text-center mt-4 md:float-right">
